12f1646c0431684a8af670af4b7af3d025f13033
JeromeIsntHere <jeromeisnthere@gmail.com>
2022-02-14 19:09:57 +0800

    Base File

---
+++ Assignment2.1_P01_2000251E_JeromeLimYuxuan/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,54 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerManager : MonoBehaviourPunCallbacks
+{
+  // We will instantiate the prefab using the name
+  // of the prefab.
+  public string mPlayerPrefabName;
+
+  // We keep a reference to the spawnpoints component.
+  // This is required to spawn our player at runtime.
+  public PlayerSpawnPoints mSpawnPoints;
+
+  // This is the game object created from the prefab name.
+  private GameObject mPlayerGameObject;
+
+  // We will create out third-person camera from
+  // this script and bind it to the camera at runtime.
+  private ThirdPersonCamera mThirdPersonCamera;
+
+    private void Start()
+  {
+    CreatePlayer();
+  }
+
+
+  public void CreatePlayer()
+  {
+    mPlayerGameObject = PhotonNetwork.Instantiate(mPlayerPrefabName,
+        mSpawnPoints.GetSpawnPoint().position,
+        mSpawnPoints.GetSpawnPoint().rotation,
+        0);
+
+        mPlayerGameObject.GetComponent<PlayerMovement>().mFollowCameraForward = false;
+        mThirdPersonCamera = Camera.main.gameObject.AddComponent<ThirdPersonCamera>();
+        mThirdPersonCamera.mainPlayerTransform = mPlayerGameObject.transform;
+        mThirdPersonCamera.mainCameraTransform = Camera.main.transform;
+        mThirdPersonCamera.inspDamping = 5.0f;
+        mThirdPersonCamera.myCameraSelection = ThirdPersonCamera.CameraSelection.TrackPositionAndRotation;
+    }
+  public void OnClick_LeaveRoom()
+  {
+    Debug.LogFormat("LeaveRoom");
+    PhotonNetwork.LeaveRoom();
+  }
+  public override void OnLeftRoom()
+  {
+    Debug.LogFormat("OnLeftRoom()");
+    SceneManager.LoadScene("Menu");
+  }
+}
